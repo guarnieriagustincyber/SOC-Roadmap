4e2c5cff0e3e96b7518cf611f122ae94c7a3bb0b
Guarnieri Agustin <guarnieriagustincyber@gmail.com>
2026-06-13 22:59:07 -0300

    Add files via upload

---
+++ Fase_1_Fundamentos/Semana_1_logica_Binaria/Dia_1_lab_setup/Ejercicio_3.py
@@ -0,0 +1,21 @@
+def contar_commit_autor(historial_ejemplo, auto_buscado)
+    nombre = ''
+    clave = False
+    cantidad = 0
+    palabra = ''
+    for caracter in historial
+
+         if clave:
+            nombre += caracter
+        
+        if caracter == ' ':
+            clave  = True
+        elif caracter == " | ":
+            break
+    
+for caracter in historial
+    if caracter != '':
+        cantidad += 1
+    else
+        
+ 
